421dc3bd0208112830b10c8de0fb3c1315709c77
jesusoterove <jesusoterove@gmail.com>
2019-08-21 15:20:31 -0500

    Updated plantilla to use latest droppable changes using interact library

--- dp_salud_basico_actividad1-linea-tiempo/js/lib.js
+++ dp_salud_basico_actividad1-linea-tiempo/js/lib.js
@@ -1451,7 +1451,7 @@ dhbgApp.standard.start = function() {
     });
 
 
-    if ((!dhbgApp.scorm || !dhbgApp.scorm.lms) && dhbgApp.SHOWNOTSCORMMSG) {
+    if (dhbgApp.MODEL == 'scorm' && (!dhbgApp.scorm || !dhbgApp.scorm.lms)) {
         $('#not_scorm_msg').html(dhbgApp.s('scorm_not'));
         $('#not_scorm_msg').dialog( { modal: true } );
     }
@@ -1460,7 +1460,7 @@ dhbgApp.standard.start = function() {
         dhbgApp.scorm.activities = dhbgApp.sortObjectByProperty(dhbgApp.scorm.activities);
     }
 
-    if (dhbgApp.scorm && dhbgApp.scorm.change_sco) {
+    if (dhbgApp.MODEL == 'scorm' && dhbgApp.scorm && dhbgApp.scorm.change_sco) {
         dhbgApp.changeSco(dhbgApp.scorm.currentSco);
     }
     else {
