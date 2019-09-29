b288016c181a80ef46c24cf59b88fdad804b5c27
jesusoterove <jesusoterove@gmail.com>
2019-09-28 21:25:51 -0500

    Made progress in all elements presentation

--- tp_avanzado_juego/js/customlib.js
+++ tp_avanzado_juego/js/customlib.js
@@ -4,9 +4,15 @@
  * @author: Jesus Otero
  */
 (function(app) {
+    //var conn = new WebSocket('ws://rutatic.udea.edu.co:8080');
     var conn = new WebSocket('ws://localhost:8080');
     var connected = false;
     var usr = new Date().getTime();
+    var cases = ['john', 'natalia', 'hermes', 'santiago', 'nairobi'];
+    var role = 'media';
+    var currentCase = 0;
+    var playDeck;
+    var deckViewer;
 
 
     /**
@@ -66,6 +72,89 @@
         $mess.get(0).scrollIntoView();
     }
 
+    function setCaseNumber($el) {
+        var text = '0'+currentCase;
+        $el.find('.case-number').html(text.substr(text.length - 2));
+    }
+
+    function makeSwippable(el, options) {
+        return new Swiper(el, Object.assign({
+            slidesPerView: 'auto',
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            scrollbar: {
+                el: '.swiper-scrollbar',
+            },
+            mousewheel: false,
+            observer: true, 
+            observeParents: true
+        }, options || {}));
+    }
+
+    function loadInstructionsFor(instructionId, callback) {
+        var $inst = $(instructionId).clone().appendTo($('.instructions-container').empty());
+
+
+        if (callback) {
+            callback($inst);
+        }
+    }
+
+    function loadCase() {
+        var name = cases[currentCase++];
+        loadInstructionsFor('#case-'+name, setCaseNumber);
+
+        //prepare play deck
+        var $deck = $("#play-deck");
+
+        if (!playDeck) {
+            playDeck = makeSwippable($deck.get(0));
+        }
+
+        playDeck.removeAllSlides();
+        var selector = ".dropzone[data-target-group='case-"+name+"']";
+        $(selector).each(function(i, it) {
+            //console.log($it.html());
+            playDeck.appendSlide(it.outerHTML);
+        });
+        playDeck.update();
+
+        //prepare deck-viewer
+        var $deck = $("#deck-viewer");
+
+        if (!deckViewer) {
+            deckViewer = makeSwippable($deck.get(0), { 
+                spaceBetween: 40,
+                effect: 'coverflow',
+                grabCursor: true,
+                centeredSlides: true,
+                coverflowEffect: {
+                    rotate: 50,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 1,
+                    slideShadows : true,
+                },
+            });
+        }
+
+        deckViewer.removeAllSlides();
+
+        selector = ".card."+role;
+        if (role == 'master') {
+            selector += "[data-group='case-"+name+"']";
+        }
+
+        $(selector).each(function(i, it) {
+            deckViewer.appendSlide(it.outerHTML);
+            deckViewer.update();
+        });
+        /*if (currentCase < 5)
+            setTimeout(loadCase, 5000);*/
+    }
+
     function btnSendOnClick(event) {
         sendMsg();        
     }
@@ -76,6 +165,18 @@
             return false;
         }
     }
+
+    function shuffle(arr) {
+        var i = arr.length, k1, k2;
+        //Shuffled positions
+        while(i--) {
+            k1 = Math.floor(Math.random() * i);
+            k2 = arr[k1];
+            arr[k1] = arr[i];
+            arr[i] = k2;
+        }
+    }
+
     /**
      * To handle when an activity has been rendered. It will hide verify button on 1-camea form.
      * @param {event} event
@@ -84,9 +185,19 @@
      */
     function initialize() {
 
+        //bind event handlers
         $('.btn.send').on('click', btnSendOnClick);
-
         $('.chat-input').keydown(chatInputOnEnter);
+
+        //If, it is first time, shuffle cases, perhaps this should change to choose a case randomly when the current case is completed
+        shuffle(cases);
+
+
+        //load current state
+        loadInstructionsFor('#instructions');
+
+        //Load a case
+        loadCase();
     }
     
     $(document).ready(function() {
