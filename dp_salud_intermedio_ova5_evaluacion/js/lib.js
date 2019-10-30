f97bc0beb2ab1d2fd0f8f3b2a21c161f2975ec29
cirano <davidherney@gmail.com>
2019-10-29 22:12:54 -0500

    Nuevo OVA 020315

--- dp_salud_intermedio_ova5_evaluacion/js/lib.js
+++ dp_salud_intermedio_ova5_evaluacion/js/lib.js
@@ -1601,7 +1601,7 @@ dhbgApp.standard.start = function() {
         dhbgApp.changeSco(dhbgApp.scorm.currentSco);
     }
     else {
-        dhbgApp.loadPage(1, 0);
+        dhbgApp.loadPage(0, 0);
     }
 };
 
